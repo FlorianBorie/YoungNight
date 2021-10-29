40953dd204a6899e4316e347b80b5242c5a5c443
FlorianBorie <florian.borie@ynov.com>
2021-10-29 15:27:06 +0200

    begin for the navigation in the app$

---
+++ Components/files/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Text } from "react-native"
+
+const Home = props => (
+    // <Burger navigation={props.navigation}>
+        <Text>Page Home !!</Text>
+    /* </Burger> */
+)
+
+export default Home
