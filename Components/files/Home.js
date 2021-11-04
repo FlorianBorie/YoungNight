d0133a8de4b96289032e1f48de2e65c1acc02f6b
FlorianBorie <florian.borie@ynov.com>
2021-11-04 12:26:41 +0100

    mise en place du menu et des animations

--- Components/files/Home.js
+++ Components/files/Home.js
@@ -1,17 +1,42 @@
-import React from "react"
-import { Text } from "react-native"
+import React, { useRef, useState } from 'react';
+import { View, Text, Image, SafeAreaView, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+
+// Photo
+import photo from '../../assets/aperitif.png'
 
 const titleStyle = {
     textAlign: 'center',
     fontSize: '20px',
 };
 
-const Home = props => {
+export default function Home() {
+  const [currentTab, setCurrentTab] = useState("Home");
     return (
-        // <Burger navigation={props.navigation}>
-            <Text style={titleStyle}>Page Home !!</Text>
-        /* </Burger> */
+        <View>
+            <Text style={{
+              fontSize: 30,
+              fontWeight: 'bold',
+              color: 'black',
+              paddingTop: 20
+            }}>{currentTab}</Text>
+
+            <Image source={photo} style={{
+              width: '100%',
+              height: 300,
+              borderRadius: 15, 
+              marginTop: 20
+            }}></Image>
+
+            <Text style={{
+              fontSize: 20,
+              fontWeight: 'bold',
+              paddingTop: 15,
+              paddingBottom: 5
+            }}>Apéro partie !!</Text>
+
+            <Text style={{
+            }}>Soirée dans un appartement sur Bordeaux
+            Dans le quartier des chartron !!</Text>
+        </View>
     )
 }
-
-export default Home;
