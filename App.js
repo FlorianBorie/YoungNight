a7d129afb3af379bda7f7441fd730f62436fcb77
FlorianBorie <florian.borie@ynov.com>
2021-11-05 17:17:37 +0100

    update navigation

--- App.js
+++ App.js
@@ -50,7 +50,6 @@ export default function App() {
         }} listeners={({navigation, route})=>({
           tabPress: e =>{
             Animated.spring(tabOffsetValue,{
-              toValue: 0,
               useNativeDriver: true
             }).start()
           }
@@ -72,7 +71,6 @@ export default function App() {
         }} listeners={({navigation, route})=>({
           tabPress: e =>{
             Animated.spring(tabOffsetValue,{
-              toValue:getWight(),
               useNativeDriver: true
             }).start()
           }
@@ -116,7 +114,6 @@ export default function App() {
         }} listeners={({navigation, route})=>({
           tabPress: e =>{
             Animated.spring(tabOffsetValue,{
-              toValue:getWight() * 4,
               useNativeDriver: true
             }).start()
           }
@@ -129,7 +126,7 @@ export default function App() {
               top: '50%'
             }}>
               <FontAwesome5
-              name="home"
+              name="cog"
               size={20}
               color={focused ? 'red' : 'gray'}
               ></FontAwesome5>
@@ -138,7 +135,6 @@ export default function App() {
         }} listeners={({navigation, route})=>({
           tabPress: e =>{
             Animated.spring(tabOffsetValue,{
-              toValue:getWight() * 5,
               useNativeDriver: true
             }).start()
           }
@@ -147,7 +143,6 @@ export default function App() {
       </Tab.Navigator>
 
       <Animated.View style={{
-        width: getWight() - 20,
         height: 2,
         backgroundColor: 'red',
         position: 'absolute',
@@ -165,15 +160,6 @@ export default function App() {
   )
 }
 
-function getWight() {
-  let wight = Dimensions.get("window").width
-
-  // Padding horizontal = 20
-  wight = wight - 80
-
-  // total 5 tab
-  return wight / 5
-}
 function EmptyScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -208,190 +194,3 @@ function SettingsScreen() {
     </View>
   );
 }
-
-
-
-
-
-
-
-
-
-
-// import profil from './assets/profil.png'
-// // Pages
-// import Home from './Components/files/Home';
-// // Icone menu
-// import home from './assets/home.png'
-// import search from './assets/magnifying-glass.png'
-// import notifications from './assets/notification.png'
-// import settings from './assets/setting.png'
-// import logout from './assets/logout.png'
-// // Menu
-// import menu from './assets/menu.png'
-// import close from './assets/close.png'
-
-// export default function App() {
-//   const [currentTab, setCurrentTab] = useState("Home");
-//   // Statut du menu
-//   const [showMenu, setShowMenu] = useState(false)
-//   // Animation
-//   const offsetValue = useRef(new Animated.Value(0)).current;
-//   //Initialise 1 fois
-//   const scaleValue = useRef(new Animated.Value(1)).current;
-//   const closeButtonOffset = useRef(new Animated.Value(0)).current;
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <View style={{ justifyContent: 'flex-start', padding: 15}}>
-//         <Image source={profil} style={{
-//           width: 90,
-//           height: 90,
-//           borderRadius: 10,
-//           marginTop: 8
-//         }}></Image>
-//         <Text style={{
-//           fontSize: 20,
-//           fontWeight: 'bold',
-//           color: 'white',
-//           marginTop: 20
-//         }}>YoungNight</Text>
-
-//         <TouchableOpacity>
-//           <Text style={{
-//             marginTop: 6,
-//             color: 'white'
-//           }}>Profile</Text>
-//         </TouchableOpacity>
-
-//         <View style={{ flexGrow: 1, marginTop: 50}}>
-//           {
-//             // tab bar button ..
-//           }
-//           {TabButton(currentTab, setCurrentTab, "Home", home)}
-//           {TabButton(currentTab, setCurrentTab, "Search", search)}
-//           {TabButton(currentTab, setCurrentTab, "Notifications", notifications)}
-//           {TabButton(currentTab, setCurrentTab, "Settings", settings)}
-
-//         </View>
-
-//         <View>
-//           {TabButton(currentTab, setCurrentTab, "Logout", logout)}
-//         </View>  
-//       </View>
-
-//       {
-//         // Over lay view
-//       }
-
-//       <Animated.View style={{
-//         flexGrow: 1,
-//         backgroundColor: 'white',
-//         position: 'absolute',
-//         top: 0,
-//         bottom: 0,
-//         left: 0,
-//         right: 0,
-//         paddingHorizontal: 15,
-//         paddingVertical: 20,
-//         borderRadius: showMenu ? 15 : 0,
-//         // Tranformation de la vue
-//         transform:[
-//           { scale: scaleValue },
-//           {translateX: offsetValue}
-//         ]
-//       }}>
-//         {
-//           // Boutton menu
-//         }
-
-//         <Animated.View style={{
-//           transform:[{
-//             translateY: closeButtonOffset
-//           }]
-//         }}>
-//           <TouchableOpacity onPress={() =>{
-//             Animated.timing(scaleValue, {
-//               toValue: showMenu ? 1 : 0.88,
-//               duration: 300,
-//               useNativeDriver: true
-//             })
-//             .start()
-
-//             Animated.timing(offsetValue, {
-//               toValue: showMenu ? 0 : 230,
-//               duration: 300,
-//               useNativeDriver: true
-//             })
-//             .start()
-
-//             Animated.timing(closeButtonOffset, {
-//               toValue: !showMenu ? -30 : 0,
-//               duration: 300,
-//               useNativeDriver: true
-//             })
-//             .start()
-
-//             setShowMenu(!showMenu);
-//           }}>
-
-//             <Image source={showMenu ? close : menu} style={{
-//               width: 40,
-//               height: 40,
-//               tintColor: 'black',
-//               marginTop: 40
-//             }}></Image>
-
-//           </TouchableOpacity>
-//             <Home/>
-//         </Animated.View>  
-//       </Animated.View>
-//     </SafeAreaView>
-//   )
-// }
-
-// const TabButton = (currentTab, setCurrentTab, title, image) => { 
-//   return(
-
-//     <TouchableOpacity onPress={() =>{
-//       if (title == "Logout") {
-
-//       }else{
-//         setCurrentTab(title)
-//       }
-//     }}>
-//             <View style={{
-//               flexDirection: "row",
-//               alignItems: 'center',
-//               paddingVertical: 8,
-//               backgroundColor: currentTab == title ? 'white' : 'transparent',
-//               paddingLeft: 13,
-//               paddingRight: 35,
-//               borderRadius: 8,
-//               marginTop: 10
-//             }}>
-//               <Image source={image} style={{
-//                 width: 25, height: 25,
-//                 tintColor: currentTab == title ? "#5359D1" : "white"
-//               }}></Image>
-
-//               <Text style={{
-//                 fontSize: 15,
-//                 fontWeight: 'bold',
-//                 paddingLeft: 15,
-//                 color: currentTab == title ? "#5359D1" : "white"
-//               }}>
-//                 {title}
-//               </Text>
-//             </View>
-//           </TouchableOpacity>
-//   )
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#5359D1',
-//     alignItems: 'flex-start',
-//     justifyContent: 'flex-start',
-//   },
-// });
