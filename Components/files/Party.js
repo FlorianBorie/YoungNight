198286c160a4c74c198c59cdcff527de5e10555c
Guillaume ANTON <anton.guillaume0@gmail.com>
2021-12-02 10:22:11 +0100

    modif party

--- Components/files/Party.js
+++ Components/files/Party.js
@@ -1,12 +1,11 @@
 import React, { useRef, useState, useEffect, Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, ScrollView } from 'react-native';
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 
 const partystyle = {
   padding: 20,
   alignItems: "center",
-  backgroundColor: "red",
   marginTop: 60,
 };
 
@@ -28,24 +27,49 @@ class Party extends Component {
     super(props);
     this.getParty();
   }
+
+  // Affichage des partie enregistrer dans le cloud FireStore
+
   getParty = async () => {
     // const UserParty = await getFirestore().collection("party").doc('0IDxMtQxuk7zAmBBRnAQ').get()
-    // const user = doc(getFirestore(), "party", 'xedDXR3fljBnGX7LaBfy');
-    const user = await firestore().collection('party').doc('xedDXR3fljBnGX7LaBfy').get();
+    const user = doc(getFirestore(), "party", 'xedDXR3fljBnGX7LaBfy');
+    // const user = await firestore().collection('party').doc('xedDXR3fljBnGX7LaBfy').get();
     // this.setState({
     //   name: party.data.name
     // })
-    const party = await getDoc(user)
-    console.log(party.data())
+     const party = await getDoc(user)
+     const partyData = party.data()
+     console.log(partyData)
   } 
   render() {
   return(
-  <View style={partystyle}>
+  <ScrollView >
+    <View style={partystyle}>
       <Text style={inputStyle1} >Nom : {}</Text>
       <Text style={inputStyle1} >Localisation : {}</Text>
       <Text style={inputStyle1} >Type de soirée : {this.getParty.party}</Text>
       <Text style={inputStyle1} >Nombre personnes : {this.getParty.party}</Text>
-  </View>
+    </View>
+    <View style={partystyle}>
+      <Text style={inputStyle1} >Nom : {}</Text>
+      <Text style={inputStyle1} >Localisation : {}</Text>
+      <Text style={inputStyle1} >Type de soirée : {this.getParty.party}</Text>
+      <Text style={inputStyle1} >Nombre personnes : {this.getParty.party}</Text>
+    </View>  
+    <View style={partystyle}>
+      <Text style={inputStyle1} >Nom : {}</Text>
+      <Text style={inputStyle1} >Localisation : {}</Text>
+      <Text style={inputStyle1} >Type de soirée : {this.getParty.party}</Text>
+      <Text style={inputStyle1} >Nombre personnes : {this.getParty.party}</Text>
+    </View>  
+    <View style={partystyle}>
+      <Text style={inputStyle1} >Nom : {}</Text>
+      <Text style={inputStyle1} >Localisation : {}</Text>
+      <Text style={inputStyle1} >Type de soirée : {this.getParty.party}</Text>
+      <Text style={inputStyle1} >Nombre personnes : {this.getParty.party}</Text>
+    </View> 
+  </ScrollView>
+  
   )
   }
 }
